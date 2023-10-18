7f7aefc38d1fbf22289ec5f813618be3636f208e
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2023-10-18 09:57:58 -0600

    product post added

--- index.js
+++ index.js
@@ -28,12 +28,42 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection =client.db("automotive").collection("servicesProduct");
+
+    // post product in database 
+   app.post('/product',async(req,res)=>{
+      const product = req.body;
+      console.log(product)
+      const result= await productCollection.insertOne(product);
+      res.send(result)
+   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
